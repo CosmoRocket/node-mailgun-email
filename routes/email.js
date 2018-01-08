63e79218ae5503549634e7c9033b88b225059e62
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-08 23:27:00 +1100

    Add sendEmail function to /email route

--- routes/email.js
+++ routes/email.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const { sendEmail } = require('../mailgun/mailgunClient')
 
 // POST - Send an E-mail
 router.post('/email', (req, res) => {
   const attributes = req.body
   console.log(attributes)
+
+  sendEmail(attributes.recipients, attributes.subject, attributes.text, attributes.html)
+    .then(data => {
+      res.status(201).json({ data })
+    })
+    .catch(error => {
+      res.status(400).json({ error })
+    })
 })
 
 module.exports = router
